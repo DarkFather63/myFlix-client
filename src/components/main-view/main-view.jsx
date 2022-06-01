b3d5e125c67d83cfd2df6ac7d95264e6ac7055e6
DarkFather63 <erynlcraig@gmail.com>
2022-06-01 10:23:24 -0700

    Added missing parentheses

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -43,7 +43,7 @@ export class MainView extends React.Component {
     return (
       <div className='main-view'>
         {selectedMovie
-          ? <MovieView movie={selectedMovie} onBackClick={() => { this.setSelectedMovie(newSelectedMovie) }} />
+          ? (<MovieView movie={selectedMovie} onBackClick={() => { this.setSelectedMovie(newSelectedMovie) }} />)
           : movies.map(movie => (
             <MovieCard key={movie._id} movieData={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }} />
           ))
