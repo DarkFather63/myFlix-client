2e7dc217046b3666ccd03915db09cdcd56d16ad7
DarkFather63 <erynlcraig@gmail.com>
2022-06-07 15:31:07 -0700

    Update profile-view.jsx

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -8,16 +8,31 @@ export class ProfileView extends React.Component {
     const { profile, onBackClick } = this.props;
     return (
       <div className='profile-view'>
-        <div className='profile-image'>
-          <img src={user.ImagePath} />
+        <div>
+          <p>User: (user.Username)</p>
+          <p>Email: (user.Email)</p>
         </div>
-        <div className='username'>
-          <span className='value'>{username}</span>
-        </div>
-        <div className='personal-bio'>
-          <span className='label'>Bio:</span>
-          <span className='value'>{user}</span>
-        </div>
+        <h2>Favorite Movies:</h2>
+        {favoriteMovieList.map((movies) => {
+          return (
+            <div key={movies._id}>
+              <img src={movies.ImagePath} />
+              <Link to={`/movies/${movies._id}`}>
+                <h4>{movies.Title}</h4>
+              </Link>
+              <button variant="secondary" onClick={() => removeFav(movies._id)}>Remove</button>
+            </div>
+          )
+        })}
+
+        <form className='profile-form' onSubmit={(e) => handleSubmit(e)}>
+          <h2>Want to change some info?</h2>
+          <label>Username:</label>
+          <input type='text' name='Username' defaultValue={user.Username} onChange={e => handleUpdate(e)} />
+          <label>Password</label>
+          <input type='password' name='Password' defaultValue={user.Password} onChange={e => handleUpdate(e)} />
+        </form>
+
         <Link to={`/movies/${movie._id}`}>
           <Button variant="link">Movies</Button>
         </Link>
